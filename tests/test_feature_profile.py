042cc8c7b729ecd8a7c7bb2e2c9f87e0e6bcc1a8
Anton Alisevich <alisevicha@mail.ru>
2024-10-23 17:03:13 +0300

    add 1 more test

--- tests/test_feature_profile.py
+++ tests/test_feature_profile.py
@@ -18,12 +18,19 @@ class TestProfileFeature(BaseTest):
         self.login_page.click_login_button()
         self.dashboard_page.click_my_info_button()
         self.personal_page.is_opened()
-        time.sleep(5)
         self.personal_page.change_first_name(f"Test {random.randint(1, 1000)}")
-        time.sleep(5)
         self.personal_page.save_changes()
         self.personal_page.is_changes_saved()
         self.personal_page.make_screenshot("Saved Edited name")
 
-
+    @allure.title("Open Profile page")
+    @allure.severity("High")
+    @pytest.mark.regression
+    def test_open_profile_page(self):
+        self.login_page.open()
+        self.login_page.enter_login(self.data.LOGIN)
+        self.login_page.enter_password(self.data.PASSWORD)
+        self.login_page.click_login_button()
+        self.dashboard_page.click_my_info_button()
+        self.personal_page.is_opened()
 
